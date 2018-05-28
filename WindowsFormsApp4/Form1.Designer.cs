8ad1c444e7d4abd4356353103023a6ab497c828c
Rafaraxis <35494391+Rafaraxis@users.noreply.github.com>
2018-05-28 00:15:52 -0700

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class juego
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tuboarriba = new System.Windows.Forms.PictureBox();
+            this.tuboabajo = new System.Windows.Forms.PictureBox();
+            this.jugador = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.puntios = new System.Windows.Forms.Label();
+            this.reprobado = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboarriba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboabajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jugador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reprobado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tuboarriba
+            // 
+            this.tuboarriba.BackColor = System.Drawing.Color.Transparent;
+            this.tuboarriba.Image = global::WindowsFormsApp4.Properties.Resources.tubo2;
+            this.tuboarriba.Location = new System.Drawing.Point(139, -84);
+            this.tuboarriba.Name = "tuboarriba";
+            this.tuboarriba.Size = new System.Drawing.Size(102, 258);
+            this.tuboarriba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tuboarriba.TabIndex = 0;
+            this.tuboarriba.TabStop = false;
+            // 
+            // tuboabajo
+            // 
+            this.tuboabajo.BackColor = System.Drawing.Color.Transparent;
+            this.tuboabajo.Image = global::WindowsFormsApp4.Properties.Resources.tubo1;
+            this.tuboabajo.Location = new System.Drawing.Point(134, 319);
+            this.tuboabajo.Name = "tuboabajo";
+            this.tuboabajo.Size = new System.Drawing.Size(107, 311);
+            this.tuboabajo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tuboabajo.TabIndex = 1;
+            this.tuboabajo.TabStop = false;
+            // 
+            // jugador
+            // 
+            this.jugador.Image = global::WindowsFormsApp4.Properties.Resources.tenorio;
+            this.jugador.Location = new System.Drawing.Point(26, 217);
+            this.jugador.Name = "jugador";
+            this.jugador.Size = new System.Drawing.Size(64, 74);
+            this.jugador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.jugador.TabIndex = 2;
+            this.jugador.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.Form1_Load);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            // 
+            // puntios
+            // 
+            this.puntios.AutoSize = true;
+            this.puntios.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntios.Location = new System.Drawing.Point(48, 198);
+            this.puntios.Name = "puntios";
+            this.puntios.Size = new System.Drawing.Size(15, 16);
+            this.puntios.TabIndex = 3;
+            this.puntios.Text = "0";
+            // 
+            // reprobado
+            // 
+            this.reprobado.Image = global::WindowsFormsApp4.Properties.Resources.r;
+            this.reprobado.Location = new System.Drawing.Point(0, 429);
+            this.reprobado.Name = "reprobado";
+            this.reprobado.Size = new System.Drawing.Size(291, 42);
+            this.reprobado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.reprobado.TabIndex = 4;
+            this.reprobado.TabStop = false;
+            // 
+            // juego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.galeria_inicio_grande1;
+            this.ClientSize = new System.Drawing.Size(292, 469);
+            this.Controls.Add(this.reprobado);
+            this.Controls.Add(this.puntios);
+            this.Controls.Add(this.jugador);
+            this.Controls.Add(this.tuboabajo);
+            this.Controls.Add(this.tuboarriba);
+            this.Name = "juego";
+            this.Text = "Flappy ULSA";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.juego_Keypress);
+            ((System.ComponentModel.ISupportInitialize)(this.tuboarriba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tuboabajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jugador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reprobado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox tuboarriba;
+        private System.Windows.Forms.PictureBox tuboabajo;
+        private System.Windows.Forms.PictureBox jugador;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label puntios;
+        private System.Windows.Forms.PictureBox reprobado;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Dsp;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApp4
+{
+    public partial class juego : Form
+    {
+        int contadormovimiento = 5;
+        bool volararriba = false;
+        int distancia = 0;
+        Random posicionrandom = new Random();
+        WaveIn wavein;
+        WaveFormat formato;
+       
+
+        public juego()
+        {
+            InitializeComponent();
+            //teclado espacio
+            this.KeyPreview = true;
+            //cuando el juego comienze
+            iniciarjuego();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Inicializar wavein
+            wavein = new WaveIn();
+            wavein.WaveFormat = new WaveFormat(44100, 16, 1);
+            formato = wavein.WaveFormat;
+
+            wavein.DataAvailable += OnDaraAvailable;
+            
+            wavein.StartRecording();
+
+            //Cuando el tubo se mueva aparecere los nuevos tubos.
+            if (tuboabajo.Location.X > -140)
+            {
+                tuboabajo.Location = new Point((tuboabajo.Location.X) - 1, tuboabajo.Location.Y);
+                tuboarriba.Location = new Point((tuboarriba.Location.X) - 1, tuboarriba.Location.Y);
+            }
+            else
+            {
+                //en la palabra distancia es donde apareceran los nuevos tubos con diferente lugar 
+                distancia = posicionrandom.Next(-170, 118);
+                tuboabajo.Location = new Point(400, 319 + distancia);
+                tuboarriba.Location = new Point(400, -173 + distancia);
+            }
+        }
+        void OnDaraAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+
+
+            double nummuestras = bytesGrabados / 2;
+            int exponente = 1;
+            int numeroMuestrasComplejas = 0;
+            int bitsMaximos = 0;
+            do
+            {
+                bitsMaximos = (int)Math.Pow(2, exponente);
+                exponente++;
+
+            } while (bitsMaximos < nummuestras);
+
+
+
+            exponente -= 2;
+            numeroMuestrasComplejas = bitsMaximos / 2;
+
+            Complex[] muestrasComplejas =
+                new Complex[numeroMuestrasComplejas];
+
+            for (int i = 0; i < bytesGrabados; i += 2)
+            {
+                short muestra = (short)(buffer[i + 1] << 8 | buffer[i]);
+
+
+                float muestra32bits = (float)muestra / 32768.0f;
+               
+                if (i / 2 < numeroMuestrasComplejas)
+                {
+                    muestrasComplejas[i / 2].X = muestra32bits;
+                }
+
+
+
+
+            }
+
+            FastFourierTransform.FFT(true, exponente, muestrasComplejas);
+            float[] valoresAbsolutos =
+                new float[muestrasComplejas.Length];
+
+            for (int i = 0; i < muestrasComplejas.Length; i++)
+            {
+                valoresAbsolutos[i] = (float)
+                Math.Sqrt((muestrasComplejas[i].X * muestrasComplejas[i].X) +
+                (muestrasComplejas[i].Y * muestrasComplejas[i].Y));
+            }
+            int indiceMaximo =
+                valoresAbsolutos.ToList().IndexOf(
+                    valoresAbsolutos.Max());
+            float frecuenciaFundamental =
+                (float)(indiceMaximo * wavein.WaveFormat.SampleRate) /
+                (float)valoresAbsolutos.Length;
+            
+
+
+            if (frecuenciaFundamental > 500 && frecuenciaFundamental < 1000)
+            {
+                volararriba = true;
+            }
+
+            if (frecuenciaFundamental > 0 && frecuenciaFundamental < 499)
+            {
+                volararriba = false;
+            }
+
+
+        }
+        public void iniciarjuego()
+        {
+            jugador.Location = new Point(19, 225);
+            //Aqui le dices a la palabra distancia que siempre sea random 
+            distancia = posicionrandom.Next(-160, 118);
+            //al poner distancia a los tubos
+            tuboarriba.Location = new Point(270, -173 - distancia);
+            tuboabajo.Location = new Point(270, 319 - distancia);
+            //el numero de puntaje siempre conenzara con 0
+            puntios.Text = "0";
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            //Animacion de alas pero no se refleja 
+            int cantidadmovimientos = 55;
+            if (contadormovimiento <= cantidadmovimientos)
+            {
+                jugador.Image = Properties.Resources.tenorio;
+                contadormovimiento++;
+            }
+
+            if ((contadormovimiento > cantidadmovimientos / 2) && (contadormovimiento <= cantidadmovimientos * 2))
+            {
+                jugador.Image = Properties.Resources.tenorio;
+                contadormovimiento++;
+            }
+
+            contadormovimiento = (contadormovimiento > cantidadmovimientos * 2) ? 0 : contadormovimiento;
+
+            int ly = jugador.Location.Y;
+            int lx = jugador.Location.X;
+
+            //que se va callendo al alumno
+            if (volararriba)
+            {
+                ly = ly - 15;
+                volararriba = false;
+            }
+            else
+            {
+                ly++;
+            }
+
+            jugador.Location = new Point(jugador.Location.X, ly);
+
+            //cuando el alumno choca con un obstaculo se reiniciara el juego
+            if ((jugador.Bounds.IntersectsWith(reprobado.Bounds))||(jugador.Bounds.IntersectsWith(tuboarriba.Bounds)) || (jugador.Bounds.IntersectsWith(tuboabajo.Bounds)))
+            {
+                iniciarjuego();
+                wavein.StopRecording();
+            }
+            //el puntaje seguira al alumno
+            puntios.Location = new Point(jugador.Location.X + 30, jugador.Location.Y - 25);
+            puntios.Text = (tuboarriba.Location.X == jugador.Location.X) ? Convert.ToString
+                ((Convert.ToInt32(puntios.Text) + 1)).ToString() : puntios.Text; 
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void juego_Keypress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Space))
+            {
+                volararriba = true;
+            }
+        }
+    }
+
+}
